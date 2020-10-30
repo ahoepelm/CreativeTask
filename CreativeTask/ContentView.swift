9183f91ad54b3f76b6cbc968c49399d929c9ccba
ahoepelm <anthony.hoepelman@gmail.com>
2020-10-30 09:40:52 -0400

    Added delete capability and cleaned up content view.

--- CreativeTask/ContentView.swift
+++ CreativeTask/ContentView.swift
@@ -20,21 +20,7 @@ struct ContentView: View {
     private var items: FetchedResults<Task>
     
     var body: some View {
-        //        List {
-        //            ForEach(items) { item in
-        //                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-        //            }
-        //            .onDelete(perform: deleteItems)
-        //        }
-        //        .toolbar {
-        //            #if os(iOS)
-        //            EditButton()
-        //            #endif
-        //
-        //            Button(action: addItem) {
-        //                Label("Add Item", systemImage: "plus")
-        //            }
-        //        }
+
         VStack {
             ZStack {
                 HStack {
@@ -74,7 +60,7 @@ struct ContentView: View {
                 
             }
             .padding(.top)
-            //.padding(.bottom)
+
            
             HStack(alignment: .top) {
                 
@@ -128,45 +114,10 @@ struct ContentView: View {
         
     }
     
-    
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- CreativeTask/PendingTaskView.swift
+++ CreativeTask/PendingTaskView.swift
@@ -28,7 +28,7 @@ struct PendingTaskView: View {
                     .onTapGesture { self.selectDeselect(task) }
                     .animation(.linear(duration: 0.3))
             }
-            //.onDelete(perform: deleteItems)
+            .onDelete(perform: deleteItems)
         }
         
         .padding([.top, .leading, .trailing])
@@ -41,6 +41,22 @@ struct PendingTaskView: View {
             selection.insert(task)
         }
     }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { tasks[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
     
 }
 
